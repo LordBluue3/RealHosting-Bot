a4bbf85e530c129c4a15e22d40335ff3a18042c9
Mikael Oliveira <58037508+LordBluue3@users.noreply.github.com>
2024-01-28 23:28:35 -0300

    feat: create connection to database

---
+++ src/dao/ConnectionDAO.php
@@ -0,0 +1,40 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+class ConnectionDAO
+{
+    private $database;
+
+    private $connection;
+
+    public function __construct()
+    {
+        $this->configDatabase();
+
+        try{
+            $dns = "sqlite:{$this->database}";
+           $this->connection = new PDO($dns);
+        }catch(\Exception $e){
+            echo 'Erro ao se conectar com o banco de dados'. PHP_EOL;
+            echo $e;
+        }
+    }
+
+    private function configDatabase()
+    {
+        try{
+            $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
+            $dotenv->load();
+            $this->database = $_ENV['DB_DATABASE'];
+        }catch(\Exception $e){
+            echo 'Erro você precisa configurar o banco na .env'. PHP_EOL;
+            echo $e;
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
